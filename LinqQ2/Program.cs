20b7164144d15f266b4819bb64e8dedca206a43b
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-06 12:48:54 +0000

    backing up work done so far on learning to use Linq

---
+++ LinqQ2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LinqQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetFilesQuery();
+            GetFilesLambda();
+
+        }
+
+        private static void GetFilesQuery()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = from item in files
+                        where item.Length > 10000
+                        orderby item.Length, item.Name
+                        select new MyFileInfo
+                        {
+                            Name = item.Name,
+                            Length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+
+            Console.WriteLine("Filename\tSize\t\tCreation Date");
+
+            foreach (var item in query)
+            {
+                Console.WriteLine("{0} \t{1} bytes,\t{2}", item.Name, item.Length,item.CreationTime);
+            }
+        }
+
+        private static void GetFilesLambda()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = files
+                .Where(f => f.Length > 10000)
+                .OrderBy(f => f.Length).ThenBy(f => f.Name)
+                .Select(f => new MyFileInfo
+                {
+                    Name = f.Name,
+                    Length = f.Length,
+                    CreationTime = f.CreationTime
+                });
+
+            Console.WriteLine("Filename\tSize\t\tCreation Date");
+
+            foreach (var f in query)
+            {
+                Console.WriteLine("{0} \t{1} bytes,\t{2}", f.Name, f.Length, f.CreationTime);
+            }
+        }
+
+    }
+        public class MyFileInfo
+        {
+            public string Name { get; set; }
+            
+            public long Length { get; set; }
+
+            public DateTime CreationTime { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("{0,-30}{1:F0}  MB\t{2}", Name, Length/1000, CreationTime);
+            }
+        }
+    }
---
+++ LinqQ3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LinqQ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnonymousQuery();
+            AnonymousQueryLambda();
+        }
+
+        private static void AnonymousQuery()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = from item in files
+                        where item.Length > 10000
+                        orderby item.Length, item.Name
+                        select new
+                        {
+                            Name = item.Name,
+                            Length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+            Console.WriteLine("Filename\tSize\t\tCreation Date");
+
+            foreach (var f in query)
+            {
+                Console.WriteLine("{0} \t{1} bytes,\t{2}", f.Name, f.Length, f.CreationTime);
+            }
+
+        }
+
+        private static void AnonymousQueryLambda()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = files
+                .Where(f => f.Length > 10000)
+                .OrderBy(f => f.Length).ThenBy(f => f.Name)
+                .Select(f => new
+                {
+                    Name = f.Name,
+                    Length = f.Length,
+                    CreationTime = f.CreationTime
+                });
+            
+            Console.WriteLine("Filename\tSize\t\tCreation Date");
+
+            foreach (var f in query)
+            {
+                Console.WriteLine("{0} \t{1} bytes,\t{2}", f.Name, f.Length, f.CreationTime);
+            }
+        }
+    }
+}
