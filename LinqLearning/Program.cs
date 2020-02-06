afd18c3bde7fe069cf0b9f06a68c0353e2bfba46
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-06 11:56:43 +0000

    Add project files.

---
+++ LinqLearning/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //NumbersQuery();
+            NumbersLambda();
+
+        }
+        private static void NumbersQuery()
+        {
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+            var outputNumbers = from number in numbers
+                                where number > 5
+                                orderby number descending
+                                select number;
+            foreach (int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+        }
+
+
+        private static void NumbersLambda()
+        {
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var outputNumbers = numbers.Where(n => n > 5).OrderByDescending(n => n);
+
+            foreach (int number in outputNumbers)
+            {
+                Console.WriteLine(number.ToString());
+            }
+
+        }
+    }
+}
