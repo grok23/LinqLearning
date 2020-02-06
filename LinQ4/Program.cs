504ebbdee0c01b2d9eaeeb85821e079e8c5eac79
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-06 12:55:09 +0000

    started on q4

---
+++ LinQ4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 5, 3, 6, 10, 12, 8 };
+
+        }
+
+        private static int DoubleIt(int value)
+        {
+            Console.WriteLine("About to double the number " + value.ToString());
+            return value * 2;
+        }
+    }
+}
